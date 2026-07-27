75897cc8ff1a4fbf313a09351fbe7bbf151505e1
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2026-07-28 02:50:25 +0300

    feat: add logger module and update environment configuration with logging and JWT settings

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { QueueModule } from './queue/queue.module';
 import { MailModule } from './mail/mail.module';
 import { UploadModule } from './upload/upload.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [DatabaseModule, QueueModule, MailModule, UploadModule]
+  imports: [DatabaseModule, QueueModule, MailModule, UploadModule, LoggerModule]
 })
 export class InfrastructureModule {}
