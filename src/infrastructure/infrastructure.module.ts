45fa05a52d4619ee1755ba986b7804bda1be7446
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2026-08-04 09:19:23 +0300

    feat: add Mails and Queues modules to Infrastructure layer; include Reminders module in Modules layer

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,8 +3,17 @@ import { DatabaseModule } from './database/database.module';
 import { UploadModule } from './upload/upload.module';
 import { JwtModule } from './jwt/jwt.module';
 import { SecurityPasswordModule } from './password/password.module';
+import { QueuesModule } from './queues/queues.module';
+import { MailsModule } from './mails/mails.module';
 
 @Module({
-  imports: [DatabaseModule, UploadModule, JwtModule, SecurityPasswordModule],
+  imports: [
+    DatabaseModule,
+    UploadModule,
+    JwtModule,
+    SecurityPasswordModule,
+    QueuesModule,
+    MailsModule,
+  ],
 })
 export class InfrastructureModule {}
