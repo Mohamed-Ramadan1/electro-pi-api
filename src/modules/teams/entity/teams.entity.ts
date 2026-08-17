716b509ff56c82243d356652dc53da1e829df367
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2026-08-17 09:39:05 +0300

    feat: implement Teams module with controller, service, repository, and entity

---
+++ src/modules/teams/entity/teams.entity.ts
@@ -0,0 +1,5 @@
+import { BaseEntity } from '@common/index';
+import { Entity } from 'typeorm';
+
+@Entity('teams')
+export class TeamsEntity extends BaseEntity {}
--- src/modules/teams/teams.module.ts
+++ src/modules/teams/teams.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+// controllers imports
+import { TeamsController } from './controller/teams.controller';
+
+// services imports
+import { TeamsService } from './service/teams.service';
+
+// repository imports
+import { TeamsRepository } from './repos/teams.repo';
+
+// entity imports
+import { TeamsEntity } from './entity/teams.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TeamsEntity])],
+  controllers: [TeamsController],
+  providers: [TeamsService, TeamsRepository],
+})
 export class TeamsModule {}
